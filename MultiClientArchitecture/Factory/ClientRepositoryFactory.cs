0e848937d0fb86a2ff549aaf26db1b8feb241015
Manh Le <quang.manh@gmail.com>
2018-05-12 00:29:53 -0500

    Finished ClientFactory

--- MultiClientArchitecture/Factory/ClientRepositoryFactory.cs
+++ MultiClientArchitecture/Factory/ClientRepositoryFactory.cs
@@ -5,21 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using MultiClientArchitecture.Repository.Client.Interface;
 using MultiClientArchitecture.Repository.Admin.Interface;
+using Ninject;
 
 namespace MultiClientArchitecture.Factory
 {
 	public class ClientRepositoryFactory : IClientRepositoryFactory
 	{
 		IAdminRepository _adminRepo;
+		IKernel _kernel;
+
 		public ClientRepositoryFactory(IAdminRepository adminRepo) {
 			_adminRepo = adminRepo;
 		}
 
-		public T GetClientRepository<T>(int ClientId)
+
+		public IKernel IoCContainer { get => _kernel; set => _kernel = value; }
+
+		public T GetClientRepository<T>(int clientId) where T: IClientRepository
 		{
-			var clientRecord = _adminRepo.GetClientRecordById(ClientId);
-			
-			
+			if (_kernel == null)
+				throw new Exception("IoC container is not configured for factory");
+			var clientRecord = _adminRepo.GetClientRecordById(clientId);
+
+			T repo = IoCContainer.Get<T>();
+			repo.OfClient = clientRecord;
+			return repo;
 		}
 	}
 }
--- MultiClientArchitecture/Factory/IBaseFactory.cs
+++ MultiClientArchitecture/Factory/IBaseFactory.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MultiClientArchitecture.Repository.Client.Interface;
 using Ninject;
 
 namespace MultiClientArchitecture.Factory
 {
-	public interface IClientRepositoryFactory: IBaseFactory
+	public interface IBaseFactory
 	{
-		T GetClientRepository<T>(int clientId) where T : IClientRepository;
+		IKernel IoCContainer { get; set; }
 	}
 }
